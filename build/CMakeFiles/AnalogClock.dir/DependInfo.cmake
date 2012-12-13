2961858da1dfe1ffad3df5cfef1891c4c25ae542
FMichelD <FMichelD@gmail.com>
2012-12-13 01:13:49 -0200

    Mudança na arvore do projeto

---
+++ build/CMakeFiles/AnalogClock.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fmicheld/workspace/QT/AnalogClock/src/analogclock.cpp" "/home/fmicheld/workspace/QT/AnalogClock/build/CMakeFiles/AnalogClock.dir/src/analogclock.cpp.o"
+  "/home/fmicheld/workspace/QT/AnalogClock/build/src/include/moc_analogclock.cxx" "/home/fmicheld/workspace/QT/AnalogClock/build/CMakeFiles/AnalogClock.dir/src/include/moc_analogclock.cxx.o"
+  "/home/fmicheld/workspace/QT/AnalogClock/src/main.cpp" "/home/fmicheld/workspace/QT/AnalogClock/build/CMakeFiles/AnalogClock.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "QT_NO_DEBUG"
+  "QT_GUI_LIB"
+  "QT_CORE_LIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/AnalogClock.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/AnalogClock.dir/src/main.cpp.o"
+  "CMakeFiles/AnalogClock.dir/src/analogclock.cpp.o"
+  "CMakeFiles/AnalogClock.dir/src/include/moc_analogclock.cxx.o"
+  "src/include/moc_analogclock.cxx"
+  "AnalogClock.pdb"
+  "AnalogClock"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/AnalogClock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
